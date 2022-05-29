fdba2bb870ab5a0f5c42dcb93ea4841c331f2ba9
Jesús Mendoza <mendoza.git@gmail.com>
2022-05-29 17:32:58 -0500

    Invoice test added

--- InvoicingSamples/Form1.cs
+++ InvoicingSamples/Form1.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using Invoicing.Base;
 using Invoicing.Common;
+using Invoicing.Common.Constants;
+using Invoicing.Common.Enums;
+using Invoicing.Common.Extensions;
+using Invoicing.Common.Serializing;
 
 namespace InvoicingSamples
 {
@@ -24,8 +29,8 @@ namespace InvoicingSamples
             {
                 Tin = "MEJJ940824C61",
                 LegalName = "JESUS MENDOZA JUAREZ",
-                TaxRegime = "621" //Incorporación Fiscal
-
+                TaxRegimeId = "621", //Incorporación Fiscal,
+                OperationNumber = null
             };
 
             //receptor
@@ -33,16 +38,131 @@ namespace InvoicingSamples
             {
                 Tin = "DGE131017IP1",
                 LegalName = "DYM GENERICOS",
-                InvoiceUse = "G01"
+                ZipCode = null,
+                ForeignCountryId = null,
+                ForeignTin = null,
+                TaxRegimeId = "601", //General de Ley Personas Morales
+                CfdiUseId = "G01" //Adquisición de mercancías.
             };
+
+            var itemTaxes = new InvoiceItemTaxesWrapper
+            {
+                TransferredTaxes = new List<InvoiceItemTax>
+                {
+                    new InvoiceItemTax
+                    {
+                        SubTotal = 100,
+                        TaxId = "002",
+                        TaxTypeId = "Exento"
+                    },
+                    new InvoiceItemTax
+                    {
+                        SubTotal = 100,
+                        TaxId = "002",
+                        TaxTypeId = "Tasa",
+                        TaxRate = 0.160000m,
+                        Amount = 16
+                    },
+                    new InvoiceItemTax
+                    {
+                        SubTotal = 100,
+                        TaxId = "002",
+                        TaxTypeId = "Tasa",
+                        TaxRate = 0.160000m,
+                        Amount = 16
+                    },
+                    new InvoiceItemTax
+                    {
+                        SubTotal = 100,
+                        TaxId = "003",
+                        TaxTypeId = "Tasa",
+                        TaxRate = 0.080000m,
+                        Amount = 8
+                    }
+                },
+                WithholdingTaxes = new List<InvoiceItemTax>
+                {
+                    new InvoiceItemTax
+                    {
+                        SubTotal = 100,
+                        TaxId = "002",
+                        TaxTypeId = "Tasa",
+                        TaxRate = 0.060000m,
+                        Amount = 6
+                    }
+                }
+            };
+
+
+            //conceptos
+            var ivoiceItems = new List<InvoiceItem>()
+            {
+                new InvoiceItem
+                {
+                    SatItemId = InvoiceConstants.SatItemId,
+                    ItemId = "1801",
+                    Quantity = 1,
+                    UnitOfMeasureId = InvoiceConstants.UnitOfMeasureId,
+                    UnitOfMeasure = "PZA",
+                    Description = "Product description 1",
+                    UnitCost = 200,
+                    Amount = 200,
+                    Discount = 0,
+                    TaxObjectId = InvoiceConstants.TaxObjectId,
+                    ItemTaxex = itemTaxes
+                },
+                new InvoiceItem
+                {
+                    SatItemId = InvoiceConstants.SatItemId,
+                    ItemId = "1802",
+                    Quantity = 1,
+                    UnitOfMeasureId = InvoiceConstants.UnitOfMeasureId,
+                    UnitOfMeasure = "PZA",
+                    Description = "Product description 2",
+                    UnitCost = 200,
+                    Amount = 200,
+                    Discount = 0,
+                    TaxObjectId = InvoiceConstants.TaxObjectId,
+                    ItemTaxex = itemTaxes
+                },
+                new InvoiceItem
+                {
+                    SatItemId = InvoiceConstants.SatItemId,
+                    ItemId = "1803",
+                    Quantity = 1,
+                    UnitOfMeasureId = InvoiceConstants.UnitOfMeasureId,
+                    UnitOfMeasure = "PZA",
+                    Description = "Product description 3",
+                    UnitCost = 200,
+                    Amount = 200,
+                    Discount = 0,
+                    TaxObjectId = InvoiceConstants.TaxObjectId,
+                    ItemTaxex = itemTaxes
+                },
+                new InvoiceItem
+                {
+                    SatItemId = InvoiceConstants.SatItemId,
+                    ItemId = "1804",
+                    Quantity = 1,
+                    UnitOfMeasureId = InvoiceConstants.UnitOfMeasureId,
+                    UnitOfMeasure = "PZA",
+                    Description = "Product description 4",
+                    UnitCost = 200,
+                    Amount = 200,
+                    Discount = 0,
+                    TaxObjectId = InvoiceConstants.TaxObjectId,
+                    ItemTaxex = itemTaxes
+                }
+            };
+
 
             //comprobante
             var invoice = new Invoice
             {
-                InvoiceType = InvoiceType.I,
+                InvoiceTypeId = InvoiceType.Ingreso,
                 InvoiceDate = DateTime.Now.ToSatFormat(),
                 InvoiceVersion = InvoiceVersion.V40,
-                InvoiceSerie = InvoiceSeries.Ingreso,
+                InvoiceSerie = InvoiceSerie.Ingreso.ToValue(),
                 InvoiceNuber = "1234",
                 SignatureValue = "abcd123",
                 PaymentForm = "PUE",
@@ -50,16 +170,22 @@ namespace InvoicingSamples
                 PaymentConditions = null,
                 Subtotal = 200,
                 Discount = 0,
-                Currency = InvoiceCurrency.MXN,
+                Currency = InvoiceCurrency.MXN.ToValue(),
                 ExchangeRate = 1,
                 Total = 232,
-                PaymentMethod = "01",
+                PaymentMethodId = "01",
                 ExpeditionZipCode = "38034",
                 InvoiceRelateds = null,
-                InvoiceIssuer = null,
-                InvoiceRecipient = null,
-                InvoiceItems = null
+                InvoiceIssuer = issuer,
+                InvoiceRecipient = recipient,
+                SchemaLocation = SerializerHelper.SchemaLocationIE40,
+                InvoiceItems = ivoiceItems
             };
+
+
+            xml = Serializer<Invoice>.Serialize(invoice, SerializerHelper.NamespacesIE40,
+                SerializerHelper.DefaultXmlWriterSettings);
+            File.WriteAllText("invoice.xml", xml);
 
 
             return xml;
